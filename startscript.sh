a4666594a3dbe7f863fd7a4bfe25a8e3151bc0ff
Jean-Marc Soumet <ajmssc@macbook-air>
2024-01-01 04:18:15 -0800

    Update dockerfile

--- startscript.sh
+++ startscript.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 export dbinfo=/config/main.db
-export watchpath=/watch
-exec python3 -m realdebridmanager.FileWatch &
 exec gunicorn --bind 0.0.0.0:${rdmport:-5000} realdebridmanager.mainwebui:app
